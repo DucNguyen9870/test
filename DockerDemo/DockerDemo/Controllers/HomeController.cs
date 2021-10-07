da71c37150967e0789f9d27bd29c1be1c67a1bf8
Duc Nguyen <duc.nguyen@mogi.vn>
2021-10-07 11:30:33 +0700

    Demo Docker 2

---
+++ DockerDemo/DockerDemo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [Route("getAllNames")]
+        [HttpGet]
+        [ProducesResponseType(200,Type = typeof(string))]
+        public async Task<IActionResult> GetAllNames()
+        {
+            var result = new List<string>()
+            {
+                "Duc",
+                "Nguyen"
+            };
+
+            return Ok(result);
+        }
+    }
+}
