3603a8adca079a45a02ff27e6a93e3c2b776df10
Duc Nguyen <duc.nguyen@mogi.vn>
2021-10-15 16:06:15 +0700

    rebase

---
+++ DockerDemo/DockerDemo/Model/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerDemo.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+    }
+}
